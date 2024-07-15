93ef2176d8981f1477069fe8ac972179480da705
Pandu Purwadi <pandupurwadi18@gmail.com>
2024-07-15 14:29:32 +0700

    upload pertama

---
+++ dasar1.js
@@ -0,0 +1,80 @@
+// Cara membuat variabel
+var a = 'a'; // var sudah jarang digunakan
+let b = 'b';
+const c = 'c';
+
+//Variabel bisa menampung tipe data string (text), integer/number, boolean, array, object
+
+let nama = "bryan"; //string
+let umur = 22; //number
+let apakahBenar = true; //boolean
+let uang = 5000.5 //number namun ada koma
+
+//operator aritmatika
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 / angka2);
+console.log(angka1 * angka2);
+console.log(angka1 % angka2);
+
+// belajar mengisi variable - assignment
+
+let penampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 - angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 / angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 * angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 % angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+//untuk mengupdate variabel baru tanpa menggunakan let
+
+//string juga bisa ditambah namun string tidak bisa dikurang dengan operator aritmatika
+
+let firstname = 'agus';
+let lastname = "setiawan";
+let fullname = firstname+lastname;
+console.log(fullname);
+console.log(firstname+' '+lastname);
+
+//alur pembacaan code
+//untuk membaca code kalian harus membacanya dari kiri ke kanan, dan atas ke bawah
+
+//contoh
+
+let number = 20;
+console.log(number+10); //30
+number = 10;
+console.log(number+10); //20
+
+number = false;
+console.log(number+10); //10
+//false bernilai 0 sementara true bernilai 1
+
+//membaca error
+//error itu jangan ditakuti, justru kalian harus takut kalau ada tidak ada yang namanya error dalam pemprograman
+
+//contoh error
+// const hewan = "jerapah";
+// console.log(hewan);
+// hewan = "buaya";
+
+//const tidak bisa diubah
+//auto komen control +/
+
+//Tips! Selalu teliti dan kalau ada error, dibaca aja
+//ngoding adalah habits
+
+
+
+
